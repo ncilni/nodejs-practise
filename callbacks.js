aa5fd3c6a567c675655a7c6d35ea294ae840895a
Neeraj Chauhan <ncilni@gmail.com>
2021-06-04 20:22:00 +0530

    Promises way

--- callbacks.js
+++ callbacks.js
@@ -1,38 +1,40 @@
 console.log("Before");
 
-getUser(1, readUser);
-
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("Fetching user data from DB");
-    callback({ id: id, gitUserName: "ncilni" });
-  }, 2000);
-}
-
-function readUser(user) {
+getUser(1).then((user) => {
   console.log("User", user);
-  getRepositories(user.gitUserName, readRepositories);
+  getRepositories(user.gitUserName).then((repos) => {
+    console.log(
+      "Received repos for user named ",
+      user.gitUserName,
+      ` are ${repos}`
+    );
+    getCommits(repos[0]).then((commits) => console.log("Commits", commits));
+  });
+});
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Fetching user data from DB");
+      resolve({ id: id, gitUserName: "ncilni" });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log("sending repos for user:", username);
-    callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("sending repos for user:", username);
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
 }
 
-function readRepositories(repos) {
-  //   console.log(`Repos fetched for ${user.gitUserName} are ${repos}`);
-  getCommits(repos[0], displayCommit);
-}
-
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log("Getting commits for the repo:", repo);
-    callback(["commit1", "commit2", "commit3"]);
-  }, 2000);
-}
-
-function displayCommit(commits) {
-  console.log("Commits", commits);
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Getting commits for the repo:", repo);
+      resolve(["commit1", "commit2", "commit3"]);
+    }, 2000);
+  });
 }
