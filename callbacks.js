776562e75d54edca4434e6068c442faaf86c101c
Neeraj Chauhan <ncilni@gmail.com>
2021-06-04 21:02:17 +0530

    correction in closures

--- callbacks.js
+++ callbacks.js
@@ -1,22 +1,17 @@
 console.log("Before");
 
-getUser(1).then((user) => {
-  console.log("User", user);
-  getRepositories(user.gitUserName).then((repos) => {
-    console.log(
-      "Received repos for user named ",
-      user.gitUserName,
-      ` are ${repos}`
-    );
-    getCommits(repos[0]).then((commits) => console.log("Commits", commits));
-  });
-});
+getUser(1)
+  .then((user) => getRepositories(user.gitUserName))
+  .then((repos) => getCommits(repos[0]))
+  .then((commits) => console.log("Commits", commits))
+  .catch((err) => console.log("error", err));
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Fetching user data from DB");
       resolve({ id: id, gitUserName: "ncilni" });
+      //   reject(new Error("Unable to fetch user"));
     }, 2000);
   });
 }
