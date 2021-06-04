4fc8668c3b658031474cc4f52c7f82f51ced857c
Neeraj Chauhan <ncilni@gmail.com>
2021-06-04 20:09:39 +0530

    callback hell

--- callbacks.js
+++ callbacks.js
@@ -2,11 +2,31 @@ console.log("Before");
 
 getUser(1, function (user) {
   console.log("User", user);
+  getRepositories(user.gitUserName, function (repos) {
+    console.log(`Repos fetched for ${user.gitUserName} are ${repos}`);
+    getCommits(repos[0], (commits) => {
+      console.log("Commits", commits);
+    });
+  });
 });
+
 function getUser(id, callback) {
   setTimeout(() => {
     console.log("Fetching user data from DB");
     callback({ id: id, gitUserName: "ncilni" });
   }, 2000);
 }
-console.log("After");
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("sending repos for user:", username);
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log("Getting commits for the repo:", repo);
+    callback(["commit1", "commit2", "commit3"]);
+  }, 2000);
+}
